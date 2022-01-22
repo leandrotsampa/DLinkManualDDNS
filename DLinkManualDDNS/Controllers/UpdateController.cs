c303ba0fa49943837bf82af24d4f799584f7c8e9
lmelo <lmelo@bernhoeft.com.br>
2022-01-22 17:39:47 -0300

    First commit.

---
+++ DLinkManualDDNS/Controllers/UpdateController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Text;
+
+namespace DLinkManualDDNS.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class UpdateController : ControllerBase
+    {
+        private readonly ILogger<UpdateController> _logger;
+
+        public UpdateController(ILogger<UpdateController> logger) => _logger = logger;
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(string))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized, Type = typeof(string))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(string))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(string))]
+        public async Task<IActionResult> UpdateHost([FromQuery, Required] string hostname)
+        {
+            try
+            {
+                var userAgent = Request.Headers["User-Agent"].FirstOrDefault();
+                var clientIp = Request.HttpContext.Connection.RemoteIpAddress.MapToIPv4().ToString();
+
+                _logger.LogInformation($"[Identifier: {Request.HttpContext.TraceIdentifier}] Updating IP: {clientIp} User-Agent: {userAgent}");
+
+                // Get Username / Password.
+                var authorization = Request.Headers["Authorization"].FirstOrDefault();
+                var bytes = Convert.FromBase64String(authorization);
+                var userPwd = Encoding.ASCII.GetString(bytes).Split(':');
+
+                using var httpClient = new HttpClient(new HttpClientHandler()
+                {
+                    Credentials = new NetworkCredential(userPwd[0], userPwd[1]),
+                });
+
+                var response = await httpClient.GetStringAsync($"https://domains.google.com/nic/update?hostname={hostname}&myip={clientIp}");
+                _logger.LogInformation($"[Identifier: {Request.HttpContext.TraceIdentifier}] Response: {response}");
+
+                return response switch
+                {
+                    string a when a.StartsWith("good") => StatusCode(StatusCodes.Status200OK, "The update was successful!"),
+                    string b when b.StartsWith("nochg") => StatusCode(StatusCodes.Status200OK, $"The supplied IP address {clientIp} is already set for this host."),
+                    string c when c.StartsWith("nohost") => StatusCode(StatusCodes.Status404NotFound, $"The hostname does not exist, or does not have Dynamic DNS enabled.\nHostname: {hostname}"),
+                    string d when d.StartsWith("badauth") => StatusCode(StatusCodes.Status401Unauthorized, "The username / password combination is not valid for the specified host!"),
+                    string e when e.StartsWith("notfqdn") => StatusCode(StatusCodes.Status400BadRequest, $"The supplied hostname is not a valid fully-qualified domain name!\nHostname: {hostname}"),
+                    string f when f.StartsWith("badagent") => StatusCode(StatusCodes.Status400BadRequest, "Your Dynamic DNS client is making bad requests. Ensure the user agent is set in the request, and that you’re only attempting to set an IPv4 address. IPv6 is not supported."),
+                    string g when g.StartsWith("abuse") => StatusCode(StatusCodes.Status401Unauthorized, "Dynamic DNS access for the hostname has been blocked due to failure to interpret previous responses correctly."),
+                    string h when h.StartsWith("911") => StatusCode(StatusCodes.Status500InternalServerError, "An error happened on our end. Wait 5 minutes and retry."),
+                    _ => StatusCode(StatusCodes.Status501NotImplemented, response)
+                };
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"[Identifier: {Request.HttpContext.TraceIdentifier}]");
+                return StatusCode(StatusCodes.Status500InternalServerError, "Internal error.");
+            }
+        }
+    }
+}
---
+++ DLinkManualDDNS/Program.cs
@@ -0,0 +1,70 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "D-Link Manual Dynamic DNS",
+        Description = "Updating Google Domains Dynamic DNS with D-Link Dynamic DNS in Manual Mode.",
+        Contact = new OpenApiContact
+        {
+            Name = "Leandro Tavares de Melo",
+            Email = "leandrotsampa@yahoo.com.br",
+            Url = new Uri("https://www.linkedin.com/in/leandrotsampa/")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "MIT",
+            Url = new Uri("https://github.com/leandrotsampa/DLinkManualDDNS/blob/master/LICENSE")
+        }
+    });
+
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme (Example: 'Bearer 12345abcdef')",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.RoutePrefix = string.Empty;
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "D-Link Manual Dynamic DNS");
+    });
+}
+
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
